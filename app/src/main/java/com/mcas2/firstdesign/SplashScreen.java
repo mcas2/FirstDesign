a2038c50a021bbe7cdd2195c76343e5f02f043e7
Mario <m@m.es>
2023-10-17 13:29:08 +0200

    Initial commit

---
+++ app/src/main/java/com/mcas2/firstdesign/SplashScreen.java
@@ -0,0 +1,14 @@
+package com.mcas2.firstdesign;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
