e21714794027bda2b69d0796fba66386a8e7b7aa
mcas2 <mcas2@tutanota.com>
2023-10-19 14:29:52 +0200

    Glide working. SplashScreen with logo, title and background.

--- app/src/main/java/com/mcas2/firstdesign/SplashScreen.java
+++ app/src/main/java/com/mcas2/firstdesign/SplashScreen.java
@@ -3,6 +3,10 @@ package com.mcas2.firstdesign;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -10,5 +14,12 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        ImageView background = findViewById(R.id.backgroundSplash);
+        Glide.with(this)
+                .load("https://images.unsplash.com/photo-1533240332313-0db49b459ad6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80")
+                .transition(DrawableTransitionOptions.withCrossFade(2000))
+                .centerCrop()
+                .into(background);
     }
 }
