9dd56c0bcd912031279a614c88f07c2b7adeb321
Mario <m@m.com>
2023-11-13 12:27:22 +0100

    AlertDialog

--- app/src/main/java/com/mcas2/firstdesign/Login.java
+++ app/src/main/java/com/mcas2/firstdesign/Login.java
@@ -1,13 +1,51 @@
 package com.mcas2.firstdesign;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
+    public TextView forgotPassword;
+    //final EditText taskEditText = new EditText(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        forgotPassword = (TextView) findViewById(R.id.forgotPasswordText);
+        forgotPassword.setOnClickListener(view -> {
+            new AlertDialog.Builder(this)
+                    .setTitle("¿Olvidaste tu contraseña?")
+                    .setMessage("Puedes recuperarla a través de tu correo electrónico.")
+                    //.setView(taskEditText)
+
+                    // Specifying a listener allows you to take an action before dismissing the dialog.
+                    // The dialog is automatically dismissed when a dialog button is clicked.
+                    .setPositiveButton(android.R.string.search_go, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            // Continue with delete operation
+                        }
+                    })
+
+                    // A null listener allows the button to dismiss the dialog and take no further action.
+                    .setNegativeButton(android.R.string.no, null)
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .show();
+        });
+    }
+
+    public void openMain(View view) {
+        Toast toast = Toast.makeText(Login.this, "Entrar al Main", Toast.LENGTH_LONG);
+        toast.show();
     }
 }
