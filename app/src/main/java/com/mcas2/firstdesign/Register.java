e5cac6ed93cf63a9ce7794b6290038e067b6c7a2
mcas2 <mcas2@tutanota.com>
2023-11-22 10:04:58 +0100

    Dynamic colors

--- app/src/main/java/com/mcas2/firstdesign/Register.java
+++ app/src/main/java/com/mcas2/firstdesign/Register.java
@@ -2,13 +2,24 @@ package com.mcas2.firstdesign;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Register extends AppCompatActivity {
+    public ImageView logo;
+    public TextView appTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        logo = (ImageView) findViewById(R.id.logoRegister);
+        appTitle = (TextView) findViewById(R.id.appTitleRegister);
+
+        logo.setColorFilter(getResources().getColor(android.R.color.primary_text_light), PorterDuff.Mode.SRC_IN);
+        appTitle.setTextColor(getResources().getColor(android.R.color.primary_text_light));
+
     }
 }
